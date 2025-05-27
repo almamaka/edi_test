c93516e9b7235b6d91ab692a1f0692f1b0fc2b78
almamaka <esztzi@gmail.com>
2025-05-27 19:43:06 +0200

    added fixed reference issues version of EDI

---
+++ Edi/Edi/Installers.cs
@@ -0,0 +1,116 @@
+﻿namespace Edi
+{
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+    using Castle.Windsor.Installer;
+    using Edi.Apps.Interfaces;
+    using Edi.Apps.ViewModels;
+    using Edi.Apps.Views.Shell;
+    using Edi.Core.Interfaces;
+    using Edi.Core.Interfaces.DocumentTypes;
+    using Edi.Core.Models.DocumentTypes;
+    using MRULib.MRU.Interfaces;
+    using MsgBox;
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// This class gets picked up by from Castle.Windsor because
+    /// it implements the <see cref="IWindsorInstaller"/> interface.
+    /// 
+    /// The <see cref="IWindsorInstaller"/> interface is used by the
+    /// container to resolve installers when calling
+    /// <see cref="IWindsorContainer"/>.Install(FromAssembly.This()); 
+    /// </summary>
+    public class Installers : IWindsorInstaller
+    {
+        #region fields
+        protected static readonly log4net.ILog Logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        #endregion fields
+
+        /// <summary>
+        /// Implements the <see cref="IWindsorInstaller"/> interface to
+        /// performs the installation in the <see cref="IWindsorContainer"/>
+        /// (performed automatically when Castle scans the containing assembly).
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="store"></param>
+        public void Install(IWindsorContainer container,
+                            IConfigurationStore store)
+        {
+
+            try
+            {
+                string fullPath = System.Reflection.Assembly.GetAssembly(typeof(Installers)).Location;
+                string dir = System.IO.Path.GetDirectoryName(fullPath);
+
+                // Do a Build Solution/Rebuild Solution to ensure that all modules have been build
+                // if you are running into this assertion being raised. You should do:
+                // Solution > Clean Solution
+                // Solution > Rebuild Solution to fix this issue
+                Debug.Assert(System.IO.File.Exists(System.IO.Path.Combine(dir, "Output.dll")));
+
+                container.Install(FromAssembly.Named(System.IO.Path.Combine(dir, "Output.dll")));
+                container.Install(FromAssembly.Named(System.IO.Path.Combine(dir, "Files.dll")));
+                container.Install(FromAssembly.Named(System.IO.Path.Combine(dir, "Edi.Documents.dll")));
+                container.Install(FromAssembly.Named(System.IO.Path.Combine(dir, @"Plugins\Log4NetTools\Log4NetTools.dll")));
+            }
+            catch (Exception exp)
+            {
+                Debug.WriteLine("A Core loader error occurred {0}", exp.Message);
+                Debug.WriteLine("Stacktrace {0}", exp.StackTrace);
+                Logger.Error(exp);
+            }
+
+            // Register shell to have a MainWindow to start up with
+            container
+                .Register(Component.For<Edi.Apps.IShell<MainWindow>>()
+                .ImplementedBy<Edi.Apps.Shell>().LifestyleTransient());
+
+            // Register MainWindow to help castle satisfy Shell dependencies on it
+            container.Register(Component.For<MainWindow>().LifestyleTransient());
+        }
+
+        /// <summary>
+        /// Installs the core modules of this application into <see cref="IWindsorContainer"/>
+        /// and returns it to continue initialization/start-up using the core modules.
+        /// </summary>
+        /// <param name="container"></param>
+        internal static void InstallWindsorCore(IWindsorContainer container)
+        {
+            // Register Messagebox service to help castle satisfy dependencies on it
+            container
+                .Register(Component.For<IMessageBoxService>()
+                .ImplementedBy<MessageBoxService>().LifestyleSingleton());
+
+            try
+            {
+                string fullPath = System.Reflection.Assembly.GetAssembly(typeof(Installers)).Location;
+                string dir = System.IO.Path.GetDirectoryName(fullPath);
+
+                container.Install(FromAssembly.Named(System.IO.Path.Combine(dir, "Edi.Core.dll")));
+                container.Install(FromAssembly.Named(System.IO.Path.Combine(dir, "Edi.Themes.dll")));
+                container.Install(FromAssembly.Named(System.IO.Path.Combine(dir, "Edi.Settings.dll")));
+            }
+            catch (Exception exp)
+            {
+                Logger.Error(exp);
+            }
+
+            container.Register(Component.For<IMRUListViewModel>().Instance(MRULib.MRU_Service.Create_List()).LifestyleSingleton());
+
+            container
+                .Register(Component.For<IAvalonDockLayoutViewModel>()
+                .ImplementedBy<AvalonDockLayoutViewModel>().LifestyleSingleton());
+
+            container
+                .Register(Component.For<IDocumentTypeManager>()
+                .ImplementedBy<DocumentTypeManager>().LifestyleSingleton());
+
+            container
+                .Register(Component.For<IFileOpenService, IApplicationViewModel>()
+                .ImplementedBy<ApplicationViewModel>().LifestyleSingleton());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,64 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/7r06lm7v5o9xq7xj?svg=true)](https://ci.appveyor.com/project/Dirkster99/edi)
+[![Release](https://img.shields.io/github/release/Dirkster99/Edi.svg)](https://github.com/Dirkster99/Edi/releases/latest)  [Continues Integration](https://github.com/Dirkster99/Edi/wiki/Testing-with-Zip-file-from-AppVeyors-Continues-Integration)
+# Edi
+
+Edi - The open source integrated development environment (IDE) based on [AvalonDock](https://github.com/Dirkster99/AvalonDock), [AvalonEdit](https://github.com/icsharpcode/AvalonEdit), and [Castle Windsor](https://github.com/castleproject/Windsor).
+
+# Screenshots
+* [File Explorer Tool Window](https://github.com/Dirkster99/Edi/wiki/File-Explorer-Tool-Window)
+* [Log4Net Document Viewer](https://github.com/Dirkster99/Edi/wiki/Log4Net-Document-Viewer)
+* [UML Editor](https://github.com/Dirkster99/Edi/wiki/UML-Diagram-Editor)
+
+![screenshot](https://raw.githubusercontent.com/Dirkster99/Docu/master/Edi/2018/RestyledOverlayButtons.png)
+![screenshot](https://raw.githubusercontent.com/Dirkster99/Docu/master/Edi/2018/Edi_Explorer_Dark.png)
+![screenshot](https://raw.githubusercontent.com/Dirkster99/Docu/master/Edi/2018/Edi_Explorer_Light.png)
+![screenshot](https://raw.githubusercontent.com/Dirkster99/Docu/master/Edi/2018/EdiUmlEditor.png)
+
+## Older Screenshots with previous Theme Definition
+![screenshot](https://github.com/Dirkster99/Docu/blob/master/Edi/MetroDark_DeepBlack.png?raw=true)
+![screenshot](https://github.com/Dirkster99/Docu/blob/master/Edi/Edi_MRU_ContextMenu.png?raw=true)
+![screenshot](https://github.com/Dirkster99/Docu/blob/master/Edi/StartPage.png?raw=true)
+![screenshot](https://github.com/Dirkster99/Docu/blob/master/Edi/MetroDark_TrueBlue.png?raw=true)
+![screenshot](https://github.com/Dirkster99/Docu/blob/master/Edi/MetroLight_BrightStandard.png?raw=true)
+![screenshot](https://github.com/Dirkster99/Docu/blob/master/Edi/Generic.png?raw=true)
+![screenshot](https://github.com/Dirkster99/Docu/blob/master/Edi/MU_MenuItems.png?raw=true)
+
+## Getting Started
+
+Dowload the source, compile and run should work out of the box (you may have to enable nuget package download).
+
+### Prerequisites
+
+This application requires .NET Framework 4 to run. It runs on Windows 7 or latter.
+
+### Installing and Deployment
+
+See https://github.com/Dirkster99/Edi-Setup/ for more details on building the x86 and x64 version of the editor.
+ 
+## Built With
+
+* [Visual Studio 2017 Community](http://www.visualstudio.com) - Visual Studio IDE
+* [.Net Framework](https://www.microsoft.com/net) - Net Framework 4
+* or get the latest Release build from [Continues Integration](https://ci.appveyor.com/project/Dirkster99/edi/build/artifacts)
+
+## Contributing
+
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [Edi Versions](https://github.com/Dirkster99/Edi/tags). 
+
+## Authors
+
+* **Dirk Bahle** - *Initial work* - [Dirkster99](https://github.com/Dirkster99)
+
+See also the list of [contributors](https://github.com/Dirkster99/Edi/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/Dirkster99/Edi/blob/master/License.md) file for details
+
+## Acknowledgments
+
+See list of third party components for [aditional acknowledgements](https://github.com/Dirkster99/Edi/wiki/List-of-Contributors)
