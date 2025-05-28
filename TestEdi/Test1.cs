9e441aa1ae21776d571622994ad5b0340aea664e
almamaka <esztzi@gmail.com>
2025-05-28 15:59:40 +0200

    added ms windows compatibility package, removed FlaUI from base project

--- TestEdi/Test1.cs
+++ TestEdi/Test1.cs
@@ -1,21 +1,61 @@
-﻿using FlaUI.UIA3;
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Tools;
+using FlaUI.UIA3;
 
 namespace TestEdi
 {
     [TestClass]
     public sealed class Test1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Application? application;
+        private UIA3Automation? automation;
+        private ConditionFactory? cf;
+        private Window? mainWindow;
+
+        [TestInitialize]
+        public void Setup()
         {
             string path = @"Release\Edi.exe";
             string basedir = AppDomain.CurrentDomain.BaseDirectory;
             string path2 = Directory.GetParent(basedir).Parent.Parent.Parent.Parent.ToString();
 
-            var application = FlaUI.Core.Application.Launch(Path.Combine(path2, path));
+            application = FlaUI.Core.Application.Launch(Path.Combine(path2, path));
 
-            var mainWindow = application.GetMainWindow(new UIA3Automation());
-            
+            automation = new UIA3Automation();
+
+            cf = automation.ConditionFactory;
+            mainWindow = Retry.WhileNull(() => application.GetMainWindow(automation), TimeSpan.FromSeconds(10)).Result;
+            if (mainWindow == null)
+            {
+                throw new Exception("Main window was not found after waiting.");
+            }
+        }
+
+        [TestMethod]
+        public void FirstTest()
+        {
+
+            Assert.IsNotNull(mainWindow, "Main window was not found");
+            Assert.IsNotNull(cf, "ConditionFactory was not initialized");
+
+            //! is used because in setup() it is sure it is not null
+            var descriptionLabel = mainWindow!.FindFirstDescendant(
+                cf!.ByAutomationId("PART_WindowTitleThumb")
+            )?.AsLabel();
+
+            Assert.IsNotNull(descriptionLabel, "Description label was not found");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (application != null && !application.HasExited)
+            {
+                application.Close();
+            }
+            automation?.Dispose();
         }
     }
 }
