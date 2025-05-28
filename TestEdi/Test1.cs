54cabb66e986446ed0eb73ecdc3183f63d613396
almamaka <esztzi@gmail.com>
2025-05-28 12:54:18 +0200

    added test project and launch app with FlaUI

---
+++ TestEdi/Test1.cs
@@ -0,0 +1,21 @@
+﻿using FlaUI.UIA3;
+
+namespace TestEdi
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"Release\Edi.exe";
+            string basedir = AppDomain.CurrentDomain.BaseDirectory;
+            string path2 = Directory.GetParent(basedir).Parent.Parent.Parent.Parent.ToString();
+
+            var application = FlaUI.Core.Application.Launch(Path.Combine(path2, path));
+
+            var mainWindow = application.GetMainWindow(new UIA3Automation());
+            
+        }
+    }
+}
